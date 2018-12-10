5f9a176b0954db5d464ee5ba036879e2d3c331c0
akj61300 <rnlwjd0613@gmail.com>
2018-12-10 13:07:08 +0900

    Kotlnx View Binding 샘플

--- app/src/main/java/com/akj/kotlinsample/MainActivity.kt
+++ app/src/main/java/com/akj/kotlinsample/MainActivity.kt
@@ -2,11 +2,17 @@ package com.akj.kotlinsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 레이아웃에 button 이라는 ID 로 선언된 뷰에 클릭 이벤트 리스너를 등록한다.
+        button1.setOnClickListener {
+            // 버튼이 클릭되었을때의 코드 작성
+        }
     }
 }
