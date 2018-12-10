4c18252ffbe1870d2928677156dc2ff6241611e7
akj61300 <rnlwjd0613@gmail.com>
2018-12-11 05:42:55 +0900

    람다를 위한 테스트 코드 추가

--- app/src/test/java/com/akj/kotlinsample/KotlinTest.kt
+++ app/src/test/java/com/akj/kotlinsample/KotlinTest.kt
@@ -98,4 +98,62 @@ class KotlinTest {
         // 두 객체의 해시코드가 같은지 비교한다.
         Assert.assertEquals(fruit1.hashCode(), fruit2.hashCode())
     }
+
+    @Test
+    fun testLambda1() {
+        // 함수의 파라미터로 람다식 전달
+        println(sum(1, 2))
+        // 람다식 반환 값으로 비교
+        Assert.assertEquals(4, sum(1, 3))
+
+        // 함수에 람다 표현식을 바로 전달
+        Assert.assertEquals(4, { x: Int, y: Int -> x * y }(2, 2))
+
+        // 람다표현식 내부에서 람다표현식을 반환
+        val exp = { x: Int, y: Int ->
+            { z: Int -> (x + y) * z }
+        }
+        // exp2 --> {z: Int -> {(3 +2) * z}}
+        val exp2 = exp(3, 2)
+        // result --> (3 + 2) * 4
+        val result = exp2(4)
+        // 결과 확인
+        Assert.assertEquals(20, result)
+    }
+
+    @Test
+    fun testCollectionApi() {
+        // 컬렉션을 만든다
+        val list = listOf(1, "2", 3, 4, 5.7, 1, 2)
+
+        // filter: 컬렉션에서 특정 조건이 맞는 항목만 추출하여 새로운 컬렉션을 만든다 --> Int 타입만 추출한다.
+        println(list.filter { item -> item is Int })
+
+        // 람다 표현식에서 파라미터가 하나인 경우 생략이 가능하다.
+        // 파라미터는 it 키워드로 접근 가능
+        println(list.filter { it is Int })
+
+        // map: 컬렉션에서 아이템을 변환하여 새로운 컬렉션을 만든다.
+        println(list.map { "value: ${it}" })
+
+        // filter 에서 반환된 컬렉션을 map 으로 변환
+        println(list.filter { it is Int }.map { "value: ${it}" })
+
+        // 아이템을 찾는다.
+        println(list.find { it is Double })
+
+        // gorup 으로 변환한다.
+        val map = list.groupBy { it.javaClass }
+        println(map)
+
+        // 컬렉션 안에 컬렉션이 있는 새로운 리스트를 만든다.
+        val list2 = listOf(listOf(1, 2), listOf(3, 4))
+        println(list2)
+
+        // map 으로 항목을 변환한다.
+        println(list2.map { "value: ${it}" })
+
+        // flatmap 으로 리스트를 평평하게 만들고 변환한다.
+        println(list2.flatMap { it.toList() })
+    }
 }
