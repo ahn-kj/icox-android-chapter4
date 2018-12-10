4fa52656a6800a64650ce48150078cbc987ec829
akj61300 <rnlwjd0613@gmail.com>
2018-12-11 02:41:24 +0900

    BmiJavaActivity, BmiKotlinActivity 와 MainActivity 연결

--- app/src/main/java/com/akj/kotlinsample/MainActivity.kt
+++ app/src/main/java/com/akj/kotlinsample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.akj.kotlinsample
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,9 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 레이아웃에 button 이라는 ID 로 선언된 뷰에 클릭 이벤트 리스너를 등록한다.
+        // button1 클릭된 경우의 이벤트 리스너 설정
         button1.setOnClickListener {
-            // 버튼이 클릭되었을때의 코드 작성
+            // Intent 로 BmiJavaActivity 를 타겟으로 지정하고 startActivity 로 실행
+            startActivity(Intent(this@MainActivity, BmiJavaActivity::class.java))
+        }
+
+        // button2 클릭된 경우의 이벤트 리스너 설정
+        button2.setOnClickListener {
+            // Intent 로 BmiKotlinActivity 를 타겟으로 지정하고 startActivity 로 실행
+            startActivity(Intent(this@MainActivity, BmiKotlinActivity::class.java))
         }
     }
 }
